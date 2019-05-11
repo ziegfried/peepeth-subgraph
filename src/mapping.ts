9781ba44ffba17f42f5f8ceae92bb4428b99a891
Siegfried Puchbauer <siegfried.puchbauer@gmail.com>
2019-05-11 03:19:32 -0700

    Added new debug event entity to record info about other function calls

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,14 +6,25 @@ import {
   TypedMap,
   Bytes,
   store,
-  EthereumCall
+  EthereumCall,
+  Address
 } from "@graphprotocol/graph-ts";
-import { Account } from "../generated/schema";
+import { Account, DebugEvent } from "../generated/schema";
 import {
   CreateAccountCall,
   UpdateAccountCall,
   TransferAccountCall,
-  ChangeNameCall
+  ChangeNameCall,
+  FollowCall,
+  UnFollowCall,
+  SetIsActiveCall,
+  ReplyCall,
+  SetNewAddressCall,
+  ShareCall,
+  PostCall,
+  SaveBatchCall,
+  CashoutCall,
+  TransferOwnershipCall
 } from "../generated/Contract/Contract";
 
 function stringValueOrNull(
@@ -147,4 +158,82 @@ export function handleTransferAccount(call: TransferAccountCall): void {
     account.save();
     store.remove("Account", id);
   }
+}
+
+function createDebugEvent(
+  call: EthereumCall,
+  fn: string,
+  message: string | null,
+  ipfsHash: string | null,
+  account2: Address | null
+): void {
+  let evt = new DebugEvent(call.transaction.hash.toHex());
+  evt.fn = fn;
+  evt.block = call.block.number.toI32();
+  evt.tx = call.transaction.hash;
+  evt.from = call.transaction.from;
+  let fromAccount = Account.load(call.transaction.from.toHex());
+  if (fromAccount !== null) {
+    evt.account = call.transaction.from.toHex();
+  }
+  if (account2 != null) {
+    let toAccount = Account.load(account2.toHex());
+    if (toAccount !== null) {
+      evt.account2 = toAccount.id;
+    }
+  }
+
+  evt.save();
+}
+
+export function handleFollow(call: FollowCall): void {
+  createDebugEvent(call, "follow", null, null, call.inputs._followee);
+}
+
+export function handleUnfollow(call: UnFollowCall): void {
+  createDebugEvent(call, "unfollow", null, null, call.inputs._followee);
+}
+
+export function handleSetActive(call: SetIsActiveCall): void {
+  createDebugEvent(
+    call,
+    "setActive",
+    call.inputs._isActive ? "(true)" : "(false)",
+    null,
+    null
+  );
+}
+
+export function handleReply(call: ReplyCall): void {
+  createDebugEvent(call, "reply", null, call.inputs._ipfsHash, null);
+}
+
+export function handleNewAddress(call: SetNewAddressCall): void {
+  createDebugEvent(
+    call,
+    "newAddress",
+    call.inputs._address.toHex(),
+    null,
+    null
+  );
+}
+
+export function handleShare(call: ShareCall): void {
+  createDebugEvent(call, "share", null, call.inputs._ipfsHash, null);
+}
+
+export function handlePost(call: PostCall): void {
+  createDebugEvent(call, "post", null, call.inputs._ipfsHash, null);
+}
+
+export function handleSaveBatch(call: SaveBatchCall): void {
+  createDebugEvent(call, "saveBatch", null, call.inputs._ipfsHash, null);
+}
+
+export function handleCashout(call: CashoutCall): void {
+  createDebugEvent(call, "cashout", null, null, null);
+}
+
+export function handleTransferOwnership(call: TransferOwnershipCall): void {
+  createDebugEvent(call, "transferOwnership", null, null, call.inputs.newOwner);
 }
